4757b7f0db1fae098c8d7e469a380a6b43693565
Srijan-Murai <srijan.murai@gmail.com>
2025-12-28 10:28:50 -0500

    swe classes

--- src/main/java/com/srijanmurai/SoftwareEngineerController.java
+++ src/main/java/com/srijanmurai/SoftwareEngineerController.java
@@ -1,4 +1,28 @@
 package com.srijanmurai;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("software-engineers")
 public class SoftwareEngineerController {
+
+    @GetMapping
+    public List<SoftwareEngineer> getEngineers(){
+        return List.of(
+                new SoftwareEngineer(
+                        1,
+                        "Srijan Murai",
+                        "Python, Java, Springboot"
+                ),
+                new SoftwareEngineer(
+                        2,
+                        "John Doe",
+                        "HTML, CSS"
+                )
+        );
+    }
 }
